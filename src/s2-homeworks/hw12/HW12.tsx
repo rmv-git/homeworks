81f5abd04756f249a62dab994e3b7f25e3c78aea
hrsk <hrsk.yegor@gmail.com>
2023-07-09 18:45:05 +0300

    fix hw 12, onChangeOption

--- src/s2-homeworks/hw12/HW12.tsx
+++ src/s2-homeworks/hw12/HW12.tsx
@@ -42,8 +42,8 @@ const HW12 = () => {
                 <SuperSelect
                     id={'hw12-select-theme'}
                     className={s.select}
-                    onChange={() => change(themeId)}
                     options={themes}
+                    onChangeOption={change}
                     // сделать переключение тем
                 />
             </div>
